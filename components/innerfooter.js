c857958dae6d14b0ad60e7017b7190b861ba05d3
Priyobrato Banik <101591754+priyobratobanik@users.noreply.github.com>
2022-07-25 10:35:18 +0530

    Add files via upload

---
+++ components/innerfooter.js
@@ -0,0 +1,61 @@
+function footerdata(){
+
+    return ` <div id="foot_div">
+             
+    <div class="container">
+        <div class="footer-col">
+          <h4>INFORMATION</h4>
+          <br>
+          <ul>
+               <li id='abt'><a href="../footer-part/about.html">About Us</a></li>
+               <li><a href="../footer-part/brand.html">Associate Brands</a></li>
+               <li><a href="../footer-part/FAQ.html">FAQ</a></li>
+               <li><a href="../footer-part/collaborate.html">Collaborative with Fab Bag</a></li>
+               <li><a href="../footer-part/contact.html">Contact Us</a></li>
+               <li><a href="../footer-part/return.html">Returns & Refunds</a></li>
+               <li><a href="../footer-part/term.html">Terms & Conditions</a></li>
+               <li><a href="../footer-part/privacy.html">Privecy Policy</a></li>
+              
+          </ul>
+
+        </div>
+
+        <div class="footer-col">
+          <h4>CUSTOMER CARE</h4>
+          <br>
+           <p>Call: <a class='cont' href="foot.html"> 022-6805-6010 </a> </p>
+           <p>(Monday to Friday : 9 AM - 7 PM).</p>
+           <p>Mail : <a class='cont' href="#"> crew@fabbag.com </a> </p>
+        </div>
+
+        <div class="footer-col">
+          <h4>NEWSLETTER</h4>
+          <br>
+          <p>Signup for the latest offers, updates and news</p>
+          <input type="text" placeholder="your email Address">
+           <button id="button">Subscribe</button>
+           <br><br>
+           <p>Don’t worry we don’t spam</p>
+        </div>
+
+    </div>
+    
+ 
+ <div id="icon">
+        <a id="f" href="https://www.facebook.com/fabbag"><i class="fab fa-facebook-f"></i></a>
+        <a id="t" href="https://twitter.com/thefabbag"><i class="fab fa-twitter"></i></a>
+        <a id="i" href="https://www.instagram.com/thefabbag/"><i class="fab fa-instagram"></i></a>
+        <a id="y" href="https://www.youtube.com/fabbag"><i class="fab fa-youtube"></i></a>
+         <br><br>
+        <p>Copyright © 2021 fabbag.com All rights reserved.</p>
+ </div>
+
+</div>  `
+
+
+}
+
+
+
+export default footerdata
+
